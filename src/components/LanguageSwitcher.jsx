28a7261e29846dc0a8b5a3ad856028c856df6649
Andrei Belskii <mrandrewer@yandex.ru>
2026-01-21 02:05:07 +0300

    Add dynamic language loading

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -6,11 +6,24 @@ import LanguageIcon from '@mui/icons-material/Language';
 const LanguageSwitcher = () => {
     const { i18n } = useTranslation();
     const [anchorEl, setAnchorEl] = useState(null);
+    const [langList, setLangList] = useState([]);
 
-    const availableLanguages = Object.keys(i18n.services.resourceStore.data);
     const currentLanguage = i18n.language.split('-')[0];
 
-    const handleClick = (event) => setAnchorEl(event.currentTarget);
+    const handleClick = async (event) => {
+        const buttonElement = event.currentTarget;
+        try {
+            const response = await fetch('/lang_list.json');
+            const data = await response.json();
+            setLangList(data)
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            console.error('Default to loaded languages:', error);
+            const availableLanguages = Object.keys(i18n.services.resourceStore.data);
+            setLangList(availableLanguages)
+        }
+        setAnchorEl(buttonElement);
+    }
     const handleClose = () => setAnchorEl(null);
 
     const changeLanguage = (lng) => {
@@ -28,7 +41,7 @@ const LanguageSwitcher = () => {
                 {currentLanguage.toUpperCase()}
             </Button>
             <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
-                {availableLanguages.map((lang) => (
+                {langList.map((lang) => (
                     <MenuItem
                         key={lang}
                         onClick={() => changeLanguage(lang)}
--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,14 @@
 import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import resources from '../locales/index';
 
 i18n
     .use(LanguageDetector)
+    .use(Backend)
     .use(initReactI18next)
     .init({
-        resources: resources,
+        supportedLngs: ['en', 'ru'],
         fallbackLng: "en",
         interpolation: { escapeValue: false }
     });
